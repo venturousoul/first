89dc87fee9ba0d02983727a89d1c75cc51c0a704
tony stark <mehul.chopra.dev@gmail.com>
2018-01-13 16:36:47 +0530

    added two more functions for timestamp and month

--- date-util.py
+++ date-util.py
@@ -1,3 +1,7 @@
 # a function that will return only the current date
 
 # a function that will return the current year
+
+# a function that will return the timestamp
+
+# a function that will return the current month
