00c3dfe083449fb21fae245593da9132585d05f6
venturousoul <35060526+venturousoul@users.noreply.github.com>
2018-01-19 17:54:08 +0530

    deleting the AB.py file and changes to greetings

--- AB.py
+++
@@ -1,7 +0,0 @@
-print "Add name AB"
-'''Python math commands addition'''
-def func():
-    x = y
-    x = 9
-    print y
-'''End of Python command'''
--- greetings.py
+++ greetings.py
@@ -5,7 +5,3 @@ print 'Good afternoon'
 print 'Good evening'
 
 print 'Good night'
-
-print "Great beginning to study sessions"
-
-print "new message to be updated on the remote branch"
